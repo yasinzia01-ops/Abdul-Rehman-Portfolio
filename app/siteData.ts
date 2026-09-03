49e2ab0c66379a09e7e2df5bcafef35812753438
yasinzia01-ops <yasinzia66@gmail.com>
2026-09-03 16:45:49 +0500

    Set real Upwork profile link in footer

--- app/siteData.ts
+++ app/siteData.ts
@@ -8,7 +8,7 @@ export const site = {
   tagline: "Paid Ads & Lead Gen Specialist", // shown under the logo wordmark
   email: "hamzaabdurrehman18@gmail.com",
   bookCallSubject: "Free 20-min strategy call",
-  upwork: "https://www.upwork.com/freelancers/~your-id", // TODO: replace with your real Upwork profile URL
+  upwork: "https://www.upwork.com/freelancers/~01138f396ecb532772",
   calendly: "https://calendly.com/hamzaabdurrehman18/30min",
   capacityNote: "Taking on 2 new clients this month", // TODO: update monthly
 };
